337fa47206f9ea7fa0e1b68e96c4dd26dbcc492f
root <root@ubuntu20.04>
2021-07-04 12:23:04 +0200

    update

--- contracts/doko.sol
+++ contracts/doko.sol
@@ -4,16 +4,24 @@ pragma abicoder v2;
 
 
 
-
+import "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
 import './NonfungiblePositionManager.sol';
 
 /// @title NFT positions
 /// @notice Wraps Uniswap V3 positions in the ERC721 non-fungible token interface
 contract doko is 
- NonfungiblePositionManager
+ NonfungiblePositionManager,
+ ERC721Holder
 {
     NonfungiblePositionManager private manager;
+    mapping(uint256 => positionDetail) public tokenPosition;
 
+    struct positionDetail{
+            address token0;
+            address token1;
+            address owner;
+            uint128 liquidity;
+    }
 
     constructor( address payable NonfungiblePositionManager_addr,
         address _factory,
@@ -22,17 +30,46 @@ contract doko is
         manager = NonfungiblePositionManager(NonfungiblePositionManager_addr);
     }
 
-    function addLiqudity(MintParams memory params)
+    function deposite(uint256 tokenId, positionDetail calldata params)
         external
-        payable
         returns (
-            uint256 tokenId,
-            uint128 liquidity,
-            uint256 amount0,
-            uint256 amount1
+            bool sucess
         )
     {   
-    
-        (tokenId, liquidity, amount0, amount1) =  manager.mint(params);
+        manager.safeTransferFrom(msg.sender, address(this), tokenId);
+        tokenPosition[tokenId] = positionDetail({
+            token0 : params.token0,
+            token1 : params.token1,
+            owner : params.owner,
+            liquidity : params.liquidity 
+        });
+        sucess = true;
     }
+
+    function removeLiquidity(uint256 tokenId)   
+        external
+        payable
+        returns (uint256 amount0, uint256 amount1)
+    {
+        (amount0, amount1) = manager.decreaseLiquidity(DecreaseLiquidityParams({
+                tokenId :  tokenId,
+                liquidity : tokenPosition[tokenId].liquidity,
+                amount0Min : 0,
+                amount1Min : 0,
+                deadline : 1725230979
+        }));
+    }
+
+    function withdraw(uint256 tokenId)   
+        public
+        returns (
+            bool sucess
+        ){
+         manager.safeTransferFrom(address(this), tokenPosition[tokenId].owner,tokenId);
+         sucess = true;
+    }
+
+
+
+    
 }
